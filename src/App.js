e017de4e12ff2cfb6c273eff82bfc2f5651d4e5b
Riza Fahmi <rizafahmi@gmail.com>
2017-03-25 11:45:08 +0700

    Finish timer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  render() {
+  constructor (props) {
+    super(props)
+    this.state = {
+      intervalId: null,
+      time: 0
+    }
+  }
+  setTimer (time = 0) {
+    clearInterval(this.state.intervalId)
+    let intervalId = setInterval(() => {
+      let time = this.state.time - 1
+      if (time === 0) { clearInterval(intervalId) }
+      this.setState({
+        time
+      })
+    }, 1000)
+
+    return this.setState({
+      intervalId,
+      time
+    })
+  }
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h2>Welcome to React Timer!</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className='App-intro'>
+          <button onClick={() => this.setTimer(5)} className='five'>5 Seconds</button>
+          <button onClick={() => this.setTimer(10)} className='ten'>10 Seconds</button>
+          <button onClick={() => this.setTimer(15)} className='fiftheen'>15 Seconds</button>
         </p>
+        <h2>Time Left: {this.state.time} </h2>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
